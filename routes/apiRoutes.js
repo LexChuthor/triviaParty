0be33fe6b28e4d5d85099d603283fc44421865f0
sgliput <sgliput@comcast.net>
2018-11-15 21:43:48 -0500

    updated for test deployment

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,22 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.Question.findAll({}).then(function(dbQuestions) {
+      res.json(dbQuestions);
     });
   });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    db.Question.create(req.body).then(function(dbQuestion) {
+      res.json(dbQuestion);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Question.destroy({ where: { id: req.params.id } }).then(function(dbQuestion) {
+      res.json(dbQuestion);
     });
   });
 };
