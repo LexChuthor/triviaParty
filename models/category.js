86d149c67cee63d9c754df43697446dec190cea3
sgliput <sgliput@comcast.net>
2018-11-20 22:20:44 -0500

    updated models

---
+++ models/category.js
@@ -0,0 +1,18 @@
+module.exports = function (sequelize, DataTypes) {
+    var Category = sequelize.define("Category", {
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    Category.associate = function (models) {
+        // Associating Category with Questions
+        // When a Category is deleted, also delete any associated Questions
+        Category.hasMany(models.Question, {
+            onDelete: "cascade"
+        });
+    };
+
+    return Category;
+};
---
+++ models/question.js
@@ -0,0 +1,25 @@
+module.exports = function (sequelize, DataTypes) {
+  var Question = sequelize.define("Question", {
+    text: DataTypes.TEXT,
+    answer1: DataTypes.STRING,
+    answer2: DataTypes.STRING,
+    answer3: DataTypes.STRING,
+    answer4: DataTypes.STRING,
+    correctAnswer: DataTypes.STRING,
+    answered: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+
+  Question.associate = function (models) {
+    Question.belongsTo(models.Category, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Question;
+};
