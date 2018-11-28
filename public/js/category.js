c39623db8a181a34e9429ef099f48d3095845449
Lex Chuthor <psc9qb@gmail.com>
2018-11-27 21:13:08 -0500

    added persistent player name to category page

--- public/js/category.js
+++ public/js/category.js
@@ -1,12 +1,12 @@
+$(document).ready(function() {
+  console.log(sessionStorage.getItem("player"));
+  $(".Player1").text(sessionStorage.getItem("player"));
+  $("#score1").text(sessionStorage.getItem("score"));
+});
+
 $(".question").on("click", function() {
   $("#categoryPage").hide();
   $("#questionPage").show();
-
-
-
-
-
-
 });
 
 $(".category").on("click", function()
