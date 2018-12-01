e4a5420e38e947b9b55c21dcf38cedb14729f318
Lex Chuthor <psc9qb@gmail.com>
2018-11-30 20:19:18 -0500

    fixed endgame modal

--- public/js/category.js
+++ public/js/category.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  console.log($(".currentCat").text());
+
   console.log(sessionStorage.getItem("player"));
   console.log(sessionStorage.getItem("difficulty"));
   $(".Player1").text(sessionStorage.getItem("player"));
@@ -11,11 +11,6 @@ $(document).ready(function () {
 
     }
   }  
-  if(sessionStorage.getItem("score") == 100){
-    $('#pointsModal').modal({ backdrop: 'static', keyboard: false });
-    $(".pointsTitle").text("You have reached 100 points!");
-    $(".pointsBody").text("You may continue to play, or save your winning score now!");
-  }
 });
 
 $(".question").on("click", function () {
@@ -66,6 +61,12 @@ $(document).on("click", ".submit", function (event) {
       score = parseInt(score);
       score += 100;
       sessionStorage.setItem("score", score.toString());
+      if(sessionStorage.getItem("score") == 100){
+        $("#answerModal").modal("hide");
+        $('#pointsModal').modal({ backdrop: 'static', keyboard: false });
+        $(".pointsTitle").text("You have reached 100 points!");
+        $(".pointsBody").text("You may continue to play, or save your winning score now!");
+      }
     } else {
       $(".answerTitle").text("Sorry!");
       $(".answerBody").text(`The correct answer was actually ${response.correctAnswer}.`);
@@ -73,7 +74,7 @@ $(document).on("click", ".submit", function (event) {
     $("input[name=choices]:checked").prop('checked', false);
     $(".submitRow").empty();
     $(".submitRow").append("<button class='submit' type='submit'>Submit</button>");
-  })
+  });
 
   $.ajax("/api/questions/" + qID, {
     type: "PUT",
@@ -84,8 +85,8 @@ $(document).on("click", ".submit", function (event) {
     });
 
   $(".answerClose").on("click", function () {
-    $("#categoryPage").show();
-    $("#questionPage").hide();
+    // $("#questionPage").hide();
+    // $("#categoryPage").show();
     location.reload();
   });
 });
